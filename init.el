188b8e94e7db9002682286b341d629d4516bc35f
jaejin park <jjpark@jltech.co.kr>
2021-08-29 21:38:16 +0900

    update

--- init.el
+++ init.el
@@ -85,7 +85,7 @@
 
        :os
        ;; macos               ; recent new feature
-       ;; tty                    ;
+       tty                    ;
 
        :tools
        ;;ansible
@@ -106,7 +106,7 @@
        rgb               ; creating color strings
        taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
-       ;; tmux              ; an API for interacting with tmux
+       tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :lang
@@ -141,7 +141,7 @@
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
-       ;;lua               ; one-based indices? one-based indices
+       ;; lua               ; one-based indices? one-based indices
        (markdown +grip)          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
@@ -163,7 +163,7 @@
        rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;; (ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;; rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;; (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
@@ -173,6 +173,7 @@
        ;;terra             ; Earth and Moon in alignment for performance.
        (web +lsp)          ; the tubes
        yaml              ; JSON, but readable
+
 
        :email
        ;; (mu4e +org +gmail)
